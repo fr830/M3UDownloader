44af09979eb4bf536d82cb8c34618030af8b8b0c
Ivan Litskevich <sxkote@gmail.com>
2017-10-06 00:12:28 +0300

    before refactoring

--- M3U3Downloader/Form1.Designer.cs
+++ M3U3Downloader/Form1.Designer.cs
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.labelSource = new System.Windows.Forms.Label();
+            this.buttonGetData = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.textBoxDestination = new System.Windows.Forms.TextBox();
+            this.labelSaveTo = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSource.Location = new System.Drawing.Point(88, 56);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(390, 20);
+            this.textBoxSource.TabIndex = 0;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(484, 27);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open...";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Visible = false;
+            // 
+            // labelSource
+            // 
+            this.labelSource.AutoSize = true;
+            this.labelSource.Location = new System.Drawing.Point(12, 59);
+            this.labelSource.Name = "labelSource";
+            this.labelSource.Size = new System.Drawing.Size(63, 13);
+            this.labelSource.TabIndex = 2;
+            this.labelSource.Text = "Source File:";
+            // 
+            // buttonGetData
+            // 
+            this.buttonGetData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGetData.Location = new System.Drawing.Point(484, 54);
+            this.buttonGetData.Name = "buttonGetData";
+            this.buttonGetData.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetData.TabIndex = 3;
+            this.buttonGetData.Text = "Get Data";
+            this.buttonGetData.UseVisualStyleBackColor = true;
+            this.buttonGetData.Click += new System.EventHandler(this.buttonGetData_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(88, 82);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(390, 60);
+            this.textBoxMessage.TabIndex = 4;
+            // 
+            // textBoxDestination
+            // 
+            this.textBoxDestination.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDestination.Location = new System.Drawing.Point(88, 148);
+            this.textBoxDestination.Name = "textBoxDestination";
+            this.textBoxDestination.Size = new System.Drawing.Size(390, 20);
+            this.textBoxDestination.TabIndex = 5;
+            // 
+            // labelSaveTo
+            // 
+            this.labelSaveTo.AutoSize = true;
+            this.labelSaveTo.Location = new System.Drawing.Point(12, 151);
+            this.labelSaveTo.Name = "labelSaveTo";
+            this.labelSaveTo.Size = new System.Drawing.Size(70, 13);
+            this.labelSaveTo.TabIndex = 6;
+            this.labelSaveTo.Text = "Save To File:";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(484, 146);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 7;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExport.Location = new System.Drawing.Point(484, 175);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(75, 23);
+            this.buttonExport.TabIndex = 8;
+            this.buttonExport.Text = "Export!";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(88, 175);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(390, 23);
+            this.progressBar.TabIndex = 9;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(85, 201);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(127, 13);
+            this.labelProgress.TabIndex = 10;
+            this.labelProgress.Text = "please, start downloading";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(571, 261);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.labelSaveTo);
+            this.Controls.Add(this.textBoxDestination);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonGetData);
+            this.Controls.Add(this.labelSource);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxSource);
+            this.Name = "Form1";
+            this.Text = "M3U8 Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label labelSource;
+        private System.Windows.Forms.Button buttonGetData;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.TextBox textBoxDestination;
+        private System.Windows.Forms.Label labelSaveTo;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelProgress;
     }
 }
 
--- M3U3Downloader/Form1.cs
+++ M3U3Downloader/Form1.cs
@@ -12,9 +12,69 @@ namespace M3U3Downloader
 {
     public partial class Form1 : Form
     {
+        M3UData _data;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+
+            this.textBoxDestination.Text = sfd.FileName;
+        }
+
+        private void buttonGetData_Click(object sender, EventArgs e)
+        {
+            var source = this.textBoxSource.Text;
+            if (!source.StartsWith("http://") && !source.StartsWith("https://"))
+                source = "http://" + source;
+
+            _data = M3UData.ReadFromUrl(source);
+
+            _data.Progress += _data_Progress;
+            _data.Complete += _data_Complete;
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Segments: {_data.Segments.Count}");
+            sb.AppendLine($"Duration: {_data.Duration}");
+            sb.AppendLine($"Streams: {_data.Streams.Count}");
+            var bestStream = _data.Streams.OrderByDescending(s => s.Resolution).FirstOrDefault();
+            if (bestStream != null)
+                sb.AppendLine($"Selected Stream: R={bestStream.Resolution}: Url={bestStream.Path}");
+            this.textBoxMessage.Text = sb.ToString();
+
+            this.progressBar.Minimum = 0;
+            this.progressBar.Maximum = _data.Segments.Count;
+            this.progressBar.Value = 0;
+        }
+
+        private void _data_Complete(string message)
+        {
+            MessageBox.Show(message);
+
+            this.progressBar.Value = 0;
+        }
+
+        private void _data_Progress(int segment, int totalCount, string message)
+        {
+            this.progressBar.Value = segment > this.progressBar.Maximum ? this.progressBar.Maximum : segment;
+            this.labelProgress.Text = message;
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            if (_data == null)
+                throw new ArgumentException("No Source to download!");
+
+            if (String.IsNullOrWhiteSpace(this.textBoxDestination.Text))
+                throw new ArgumentException("No Destination File Specified!");
+
+            _data.Download(this.textBoxDestination.Text);
         }
     }
 }
